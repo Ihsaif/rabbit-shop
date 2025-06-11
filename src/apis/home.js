543a04e2d2e4b2c573247cf259233bbe4f7261ef
Ihsaif <mx1683289022@163.com>
2025-06-11 16:53:42 +0800

    home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,7 +1,18 @@
-import httpInstance from "@/utils/http"
+import httpInstance from "@/utils/http";
 
-export function getBannerAPI(){
+export function getBannerAPI() {
   return httpInstance({
-    url:'home/banner'
-  })
+    url: "home/banner",
+  });
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+  return httpInstance({
+    url: "/home/new",
+  });
+};
